fcee1118a40068bd92f9a9a06cdfce7a62a187d0
MattiaNatella <mattianatella@gmail.com>
2024-12-05 17:12:32 +0100

    [add] line 26 uppercase

--- script.js
+++ script.js
@@ -14,13 +14,16 @@ axios.get(endPoint + limit)
 
      function printCard(foto){
       const {title, url} = foto
+      // let titleCapitalize = title[0].toUppercase() + title.substr(1)
+      console.log(title[0].toUpperCase() + title.substring(1))
+      console.log(foto)
       console.log(title, url)
       return   document.querySelector('.row').innerHTML +=`<div class="col">
-      <div class="card p-3 my-1 rounded-0 mn-card">
+      <div class="card p-3 my-3 rounded-0 mn-card">
         <img src="img/pin.svg" alt="PIN">
         <img src="${url}" class="card-img-top rounded-0" alt="FOTO">
         <div class="card-body p-0 py-1">
-          <p class="card-text">${title}</p>
+          <p class="card-text">${title[0].toUpperCase() + title.substring(1)}</p>
         </div>
       </div>
     </div>`
